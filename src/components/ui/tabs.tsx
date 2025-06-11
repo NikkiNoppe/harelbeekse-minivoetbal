5637ac59537106e063529a26ab38b6fbc492e526
NikkiNoppe <nikki.noppe@student.vives.be>
2025-06-11 16:14:30 +0200

    update tab

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 import * as TabsPrimitive from "@radix-ui/react-tabs"
 
@@ -28,7 +27,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-purple-dark border-purple-light data-[state=active]:bg-purple-light data-[state=active]:text-purple-dark data-[state=active]:shadow-sm",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-purple-light text-purple-dark hover:bg-purple-dark hover:text-purple-light border-purple-light data-[state=active]:bg-purple-dark data-[state=active]:text-purple-light data-[state=active]:shadow-sm",
       className
     )}
     {...props}
